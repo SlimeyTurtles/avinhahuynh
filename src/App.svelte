91424da85be7c32a91b6dfd2e3516a24a0117eb1
Avinh Huynh <avinhahuynh@gmail.com>
2023-08-24 11:55:27 -0700

    Added a navbar and project directories

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
+	import Navbar from './components/layout/navbar.svelte';
 	export let name;
 </script>
 
+<Navbar />
+
 <main>
+	
 	<h1>Avinh's WIP Website</h1>
 	<p>email: avinhahuynh@gmail.com</p>
-	<p>phone: (619) 888 - 7408</p>
 	<p><a href="https://www.linkedin.com/in/avinh-huynh-59b075196/">LinkedIn</a></p>
 	<p><a href="https://github.com/SlimeyTurtles">Github</a></p>
 	<p><a href="https://docs.google.com/document/d/1VHEcHkoLdGqQNJh45Lq4B2C3oM5WSDVJVXhSIZYIBc4/edit?usp=sharing">Resume</a></p>
---
+++ src/components/layout/navbar.svelte
@@ -0,0 +1,45 @@
+<script>
+</script>
+
+<main>
+    <ul>
+        <li class="active"><a href="/">Avinh's Website</a></li>
+        <li><a href="news.asp">Portfolio</a></li>
+        <li><a href="contact.asp">About me</a></li>
+        <li><a href="about.asp">Contact</a></li>
+    </ul>
+</main>
+
+<style>
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #333;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+
+    li a:hover:not(.active) {
+        background-color: #111;
+    }
+
+    .active {
+        background-color: #04AA6D;
+    }
+</style>
