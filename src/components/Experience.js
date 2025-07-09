025de24116058c5f241b5d7079e157efcd2465db
Avinh Huynh <avinhahuynh@gmail.com>
2025-07-09 05:53:05 -0700

    cleaned up buttons and implemented, fixed circles so not squished

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,29 +2,31 @@ import { useState } from 'react'
 import Image from 'next/image'
 
 import experienceData from '@/data/experience.json';
+import educationData from '@/data/education.json';
 
 const Experience = () => {
     const [isExperience, setIsExperience] = useState(true);
 
-    return (
-    <div className="flex flex-col items-center p-8 bg-slate-800">
+    const dataToDisplay = isExperience ? experienceData : educationData;
 
-        <div>
-            <button onClick={() => setIsExperience(false)}>Education</button>
-            <button onClick={() => setIsExperience(true)}>Experience</button>
+    return (
+    <div className="flex flex-col items-center p-8 gap-4 bg-slate-800">
+
+        <div className="flex flex-row gap-12">
+            <button className="w-32 p-2 rounded-sm bg-slate-900 border-slate-700 border-[2px]" onClick={() => setIsExperience(true)}>Experience</button>
+            <button className="w-32 p-2 rounded-sm bg-slate-900 border-slate-700 border-[2px]" onClick={() => setIsExperience(false)}>Education</button>
         </div>
 
         <div className= "flex flex-col items-center gap-4">
-        { isExperience && experienceData.map((experience, index) => (
+        { dataToDisplay.map((experience, index) => (
             <div className="w-[60vw] bg-slate-700 flex flex-row gap-4 p-4 rounded-md" key={index}>
                 <div className="flex flex-col items-center gap-2">
-                    <div className="w-12 h-12 shrink-0 relative z-10">
+                    <div className="w-12 h-12 relative z-10 rounded-full overflow-hidden border-2 border-slate-300 bg-slate-300">
                         <Image
                             src={experience.img}
                             alt={`Logo for ${experience.company}`}
-                            width={48}
-                            height={48}
-                            className="rounded-full border-2 border-slate-300 bg-slate-300 object-cover"
+                            fill
+                            className="object-cover"
                         />
                     </div>
 
@@ -37,7 +39,7 @@ const Experience = () => {
                     <h2>{experience.company}</h2>
                     <h3>{experience.title}</h3>
                     <ul className="pl-4">
-                        { experience.items.map((item, i) => (
+                        { experience.items && experience.items.map((item, i) => (
                             <li className="list-disc" key={i}>{item}</li>
                         ))}
                     </ul>
